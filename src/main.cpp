0890e6a2058a6365f4b0e7fdac4ab8e0589d846f
Celeste <yuan.zx@outlook.com>
2022-03-05 21:34:57 +0800

    feat: log duration

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <filesystem>
 #include "marchingCubes.hpp"
@@ -14,7 +15,14 @@ int main()
     auto imgs = img_tiff::read<short>(imgFilePath);
     auto voxels = img_tiff::normalize<short, float, 255>(imgs);
 
+    auto start = std::chrono::system_clock::now();
     auto mesh = marching_cubes::extract<Vec3>(voxels, 0.5);
+    auto stop = std::chrono::system_clock::now();
+
+    std::cout << "Extract mesh complete: \n";
+    std::cout << "Time taken: " << std::chrono::duration_cast<std::chrono::hours>(stop - start).count() << " hours\n";
+    std::cout << "          : " << std::chrono::duration_cast<std::chrono::minutes>(stop - start).count() << " minutes\n";
+    std::cout << "          : " << std::chrono::duration_cast<std::chrono::seconds>(stop - start).count() << " seconds\n";
 
     auto objFilePath = std::filesystem::current_path().append(obj);
     obj::save<Vec3>(objFilePath, mesh);

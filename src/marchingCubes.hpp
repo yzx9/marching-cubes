e9304248ccf3dd7b48bedd1957d9e547496e782c
Celeste <yuan.zx@outlook.com>
2022-03-06 18:21:56 +0800

    refactor: transform algorithm functions to class

--- src/marchingCubes.hpp
+++ src/marchingCubes.hpp
@@ -1,10 +1,9 @@
 #pragma once
 #include <array>
+#include <cmath>
 #include <functional>
-#include <future>
 #include <vector>
 #include <memory>
-#include <cmath>
 #include "marchingCubesTables.hpp"
 #include "voxel.hpp"
 #include "Vec3.hpp"
@@ -22,9 +21,6 @@ namespace marching_cubes
     };
 
     template <typename T>
-    using Vertices = std::array<Vertice<T>, 8>;
-
-    template <typename T>
     class Mesh
     {
     public:
@@ -32,127 +28,119 @@ namespace marching_cubes
         std::vector<Vec3<int>> faces;
     };
 
-    namespace _private
+    template <typename T>
+    using Vertices = std::array<Vertice<T>, 8>;
+
+    template <typename T>
+    class MarchingCubes
     {
-        template <typename T>
-        void calc_voxel(const voxel::Voxels<T> &vertices, float isovalue, const Vec3<int> &pos, Mesh<T> &out);
+    public:
+        MarchingCubes(const voxel::Voxels<T> &voxels, T isovalue);
+        Mesh<T> &run();
 
-        template <typename T>
-        Vertices<T> get_vertices(const voxel::Voxels<T> &vertices, const Vec3<int> &pos);
+    private:
+        const T isovalue;
+        const voxel::Voxels<T> &voxels;
+        Mesh<T> mesh;
 
-        template <typename T>
-        std::array<int, 12> add_edge_points(const Vertices<T> &vertices, int edge, float isovalue, Mesh<T> &out);
+        void calc_voxel(const Vec3<int> &pos);
+        Vertices<T> get_vertices(const Vec3<int> &pos);
+        std::array<int, 12> add_edge_vertices(const Vertices<T> &vertices, int edge);
+    };
+
+    template <typename T>
+    Mesh<T> extract(const voxel::Voxels<T> &voxels, T isovalue)
+    {
+        MarchingCubes<T> alg(voxels, isovalue);
+        return std::move(alg.run());
     }
 
     template <typename T>
-    Mesh<T> extract(const voxel::Voxels<T> &voxels, float isovalue)
+    MarchingCubes<T>::MarchingCubes(const voxel::Voxels<T> &voxels, T isovalue) : voxels(voxels), isovalue(isovalue) {}
+
+    template <typename T>
+    Mesh<T> &MarchingCubes<T>::run()
     {
-        std::vector<std::future<Mesh<T>>> futures;
+        // TODO[feat]: support async
         for (auto x = 0; x < voxels.size() - 1; x++)
-        {
-            auto compute = [&](int x)
-            {
-                Mesh<T> mesh;
-                for (auto y = 0; y < voxels[0].size() - 1; y++)
-                    for (auto z = 0; z < voxels[0][0].size() - 1; z++)
-                        _private::calc_voxel<T>(voxels, isovalue, {x, y, z}, mesh);
+            for (auto y = 0; y < voxels[0].size() - 1; y++)
+                for (auto z = 0; z < voxels[0][0].size() - 1; z++)
+                    calc_voxel({x, y, z});
 
-                return std::move(mesh);
-            };
-
-            futures.emplace_back(std::async(compute, x));
-        }
-
-        Mesh<T> mesh;
-        for (auto &fut : futures)
-        {
-            auto submesh = fut.get();
-            auto offset = mesh.vertices.size();
-            for (auto &v : submesh.vertices)
-                mesh.vertices.emplace_back(v);
-
-            for (auto &f : submesh.faces)
-                mesh.faces.emplace_back(Vec3<int>{offset + f[0], offset + f[1], offset + f[2]});
-        }
-
-        return std::move(mesh);
+        return mesh;
     }
 
-    namespace _private
+    template <typename T>
+    void MarchingCubes<T>::calc_voxel(const Vec3<int> &pos)
     {
-        template <typename T>
-        void calc_voxel(const voxel::Voxels<T> &voxels, float isovalue, const Vec3<int> &pos, Mesh<T> &out)
-        {
-            auto v = get_vertices<T>(voxels, pos);
+        auto v = get_vertices(pos);
+        auto index = 0;
+        for (auto i = 0; i < 8; i++)
+            index |= v[i].val < isovalue ? (0x01 << i) : 0x00;
 
-            auto index = 0;
-            for (auto i = 0; i < 8; i++)
-                index |= v[i].val < isovalue ? (0x01 << i) : 0x00;
+        const auto edge = _private::edge_table[index];
+        if (edge == 0)
+            return;
 
-            const auto edge = edge_table[index];
-            if (edge == 0)
-                return;
-
-            const auto &triangle = triangle_table[index];
-            const auto points = add_edge_points<T>(v, edge, isovalue, out);
-            for (auto i = 0; triangle[i] != -1; i += 3)
-                out.faces.emplace_back(Vec3<int>{
-                    points[triangle[i + 0]],
-                    points[triangle[i + 1]],
-                    points[triangle[i + 2]]});
-        }
-
-        template <typename T>
-        Vertices<T> get_vertices(const voxel::Voxels<T> &voxels, const Vec3<int> &pos)
-        {
-            Vertices<T> v;
-            for (auto i = 0; i < 8; i++)
-            {
-                const auto &[ox, oy, oz] = vertice_offsets[i];
-                const auto x = std::get<0>(pos) + ox;
-                const auto y = std::get<1>(pos) + oy;
-                const auto z = std::get<2>(pos) + oz;
-                const auto coord = Vec3<T>{static_cast<T>(x), static_cast<T>(y), static_cast<T>(z)};
-                const auto val = voxels[x][y][z];
-                const auto normal = voxel::get_normal<T>(voxels, x, y, z);
-
-                v[i] = Vertice<T>{
-                    val : val,
-                    coord : coord,
-                    normal : normal
-                };
-            }
-
-            return v;
-        }
-
-        template <typename T>
-        std::array<int, 12> add_edge_points(const Vertices<T> &vertices, int edge, float isovalue, Mesh<T> &out)
-        {
-            std::array<int, 12> points;
-            for (auto i = 0; i < 12; i++)
-            {
-                if ((edge >> i) & 0x01)
-                {
-                    const auto &[a, b] = edge_connection[i];
-                    const auto &va = vertices[a];
-                    const auto &vb = vertices[b];
-
-                    auto coord = vec3::interpolation<T>(isovalue, va.val, vb.val, va.coord, vb.coord);
-                    auto normal = vec3::interpolation<T>(isovalue, va.normal, vb.normal);
-                    vec3::normalize(normal);
-
-                    // TODO[feat]: check if has been added
-                    points[i] = out.vertices.size();
-                    out.vertices.emplace_back(Vertice<T>{
-                        val : isovalue,
-                        coord : coord,
-                        normal : normal
-                    });
-                }
-            }
-
-            return std::move(points);
-        };
+        const auto &triangle = _private::triangle_table[index];
+        const auto edge_vertices = add_edge_vertices(v, edge);
+        for (auto i = 0; triangle[i] != -1; i += 3)
+            mesh.faces.emplace_back(Vec3<int>{
+                edge_vertices[triangle[i + 0]],
+                edge_vertices[triangle[i + 1]],
+                edge_vertices[triangle[i + 2]]});
     }
+
+    template <typename T>
+    Vertices<T> MarchingCubes<T>::get_vertices(const Vec3<int> &pos)
+    {
+        Vertices<T> v;
+        for (auto i = 0; i < 8; i++)
+        {
+            const auto &[ox, oy, oz] = _private::vertice_offsets[i];
+            const auto x = std::get<0>(pos) + ox;
+            const auto y = std::get<1>(pos) + oy;
+            const auto z = std::get<2>(pos) + oz;
+            const auto coord = Vec3<T>{static_cast<T>(x), static_cast<T>(y), static_cast<T>(z)};
+            const auto val = voxels[x][y][z];
+            const auto normal = voxel::get_normal<T>(voxels, x, y, z);
+
+            v[i] = Vertice<T>{
+                val : val,
+                coord : coord,
+                normal : normal
+            };
+        }
+
+        return v;
+    }
+
+    template <typename T>
+    std::array<int, 12> MarchingCubes<T>::add_edge_vertices(const Vertices<T> &vertices, int edge)
+    {
+        std::array<int, 12> points;
+        for (auto i = 0; i < 12; i++)
+        {
+            if (((edge >> i) & 0x01) == 0x00)
+                continue;
+
+            const auto &[a, b] = _private::edge_connection[i];
+            const auto &va = vertices[a];
+            const auto &vb = vertices[b];
+
+            auto coord = vec3::interpolation<T>(isovalue, va.val, vb.val, va.coord, vb.coord);
+            auto normal = vec3::interpolation<T>(isovalue, va.normal, vb.normal);
+            vec3::normalize(normal);
+
+            // TODO[feat]: support async
+            points[i] = mesh.vertices.size();
+            mesh.vertices.emplace_back(Vertice<T>{
+                val : isovalue,
+                coord : coord,
+                normal : normal
+            });
+        }
+
+        return std::move(points);
+    };
 }
